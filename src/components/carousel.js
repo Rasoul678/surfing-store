e88b908d213719dd1b90b2d3711f0ba0e588f137
Rasoul Hesami Rostami <h.rostami.r@gmail.com>
2023-05-11 17:06:11 +0330

    add header section

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -1,55 +1,72 @@
-export const initCarousel = () => {
+export const initCarousel = (sliderID = "#carousel") => {
   $("#left-arrow").on({ click: previousSlide });
   $("#right-arrow").on({ click: nextSlide });
-  $(".slide-image").on({ click: nextSlide });
-  $(".slide-image").on({
-    mouseenter: stopSlides,
-    mouseleave: startSlides,
-  });
+  $(`${sliderID} .slide-image`).on({ click: nextSlide });
 
   let interval = window.setInterval(rotateSlides, 3000);
 
-  function stopSlides() {
-    window.clearInterval(interval);
+  let options = {
+    threshold: [0.5],
+    root: $(`${sliderID}`)[0],
+    rootMargin: "10px",
+  };
+
+  function onEntry(entries) {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        setSlideNumber(entry.target.dataset.js);
+      }
+    });
   }
 
-  function startSlides() {
-    interval = window.setInterval(rotateSlides, 3000);
+  let observer = new IntersectionObserver(onEntry, options);
+
+  const elements = Array.from($(`${sliderID} .slide-image`));
+
+  for (let elm of elements) {
+    observer.observe(elm);
   }
+
   function rotateSlides() {
     // animation will go here
-    let $firstSlide = $("#carousel").find("div:first");
-    let width = $firstSlide.width();
+    let firstSlide = $(sliderID).find("div:first");
+    let width = firstSlide.width();
 
-    $firstSlide.animate({ marginLeft: -width }, 1000, function () {
+    // setSlideNumber(+firstSlide[0].dataset.js);
+
+    firstSlide.animate({ marginLeft: -width }, 1000, function () {
       // What to do after the animation
-      let $lastSlide = $("#carousel").find("div:last");
-      $lastSlide.after($firstSlide);
-      $firstSlide.css({ marginLeft: 0 });
+      let lastSlide = $(sliderID).find("div:last");
+      lastSlide.after(firstSlide);
+      firstSlide.css({ marginLeft: 0 });
     });
   }
 
   function nextSlide() {
     window.clearInterval(interval);
-    let $currentSlide = $("#carousel").find("div:first");
-    let width = $currentSlide.width();
-    $currentSlide.animate({ marginLeft: -width }, 1000, function () {
-      let $lastSlide = $("#carousel").find("div:last");
-      $lastSlide.after($currentSlide);
-      $currentSlide.css({ marginLeft: 0 });
+    let currentSlide = $(sliderID).find("div:first");
+    let width = currentSlide.width();
+    currentSlide.animate({ marginLeft: -width }, 1000, function () {
+      let lastSlide = $(sliderID).find("div:last");
+      lastSlide.after(currentSlide);
+      currentSlide.css({ marginLeft: 0 });
       interval = window.setInterval(rotateSlides, 3000);
     });
   }
 
   function previousSlide() {
     window.clearInterval(interval);
-    let $currentSlide = $("#carousel").find("div:first");
-    let width = $currentSlide.width();
-    let $previousSlide = $("#carousel").find("div:last");
-    $previousSlide.css({ marginLeft: -width });
-    $currentSlide.before($previousSlide);
-    $previousSlide.animate({ marginLeft: 0 }, 1000, function () {
+    let currentSlide = $(sliderID).find("div:first");
+    let width = currentSlide.width();
+    let previousSlide = $(sliderID).find("div:last");
+    previousSlide.css({ marginLeft: -width });
+    currentSlide.before(previousSlide);
+    previousSlide.animate({ marginLeft: 0 }, 1000, function () {
       interval = window.setInterval(rotateSlides, 3000);
     });
+  }
+
+  function setSlideNumber(number) {
+    $("#slide-number").html(`${number} `);
   }
 };
